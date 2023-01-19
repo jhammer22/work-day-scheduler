362ff5c17d39275af48efb8a7a67694359f15fbf
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-01-18 23:45:14 -0600

    trying to use table instead of given divs

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+
+// $(function () {
+  
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -22,4 +30,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });

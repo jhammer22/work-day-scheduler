8ae44432963b945df4788306e4e9beae4e40fc1d
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-02 17:42:42 -0600

    taking break working on color change for loop

--- script.js
+++ script.js
@@ -8,38 +8,47 @@
 var today = dayjs();
 $('#currentDay').text(dayjs().format('MMM D, YYYY h:mm A'));
 
-
-var now = parseInt(dayjs().format("H"))
+//  now gets current time parse int turns dayjs sting to integer .format gets time in 24hr time
+var now = parseInt(dayjs().format("H"));
   console.log(now); 
-  //  now gets current time parse int turns dayjs sting to integer .format gets time in 24hr time 
-
-var colorElements = document.querySelectorAll(".time-block")
-  //  querySelectorAll calls all id's with time-block and pass's them into var colorElements
-
+   
+//  querySelectorAll calls all id's with time-block and pass's them into var colorElements
+var colorElements = document.querySelectorAll(".time-block");
+console.log(colorElements);
+  
+// split method removed first part of id and only returns number as an array of string's, parseInt takes array of string's and makes them an array of integers in a new variable. Once currentTimeBlockHour was console.log found two digit integers logging last digit 10=0 11=1 etc while now is console logging 1=13 2=14 etc. When anything other than position 1 is passed into parseInt returns NaN in console, if 00 is added to id in html logged integer does not change.
 for (var i = 0; i < colorElements.length; i++) {
   var timeBlockHour = colorElements[i].id
   timeBlockHour = timeBlockHour.split("-")[1]
   var currentTimeBlockHour = parseInt(timeBlockHour.split("")[1])
   console.log(timeBlockHour)
   console.log(currentTimeBlockHour)
-} 
-// split method removed first part of id and only returns number as an array of string's, parseInt takes array of string's and makes them an array of integers in a new variable. Once currentTimeBlockHour was console.log found two digit integers logging last digit 10=0 11=1 etc while now is console logging 1=13 2=14 etc. When anything other than position 1 is passed into parseInt returns NaN in console, if 00 is added to id in html logged integer does not change. 
+}; 
+ 
 
-  // could possibly compare now integer to timeBlock string to set boolean 
-
-// let past = document.getElementById("col-8 col-md-10 description").style.backgroundColor=#d3d3d3;
-// let present = document.getElementById("col-8 col-md-10 description").style.backgroundColor=#ff6961
-// let future = document.getElementById("col-8 col-md-10 description").style.backgroundColor=#77dd77
+// could possibly compare now integer to timeBlock string to set boolean 
 // 
 // text area class= "col-8 col-md-10 description" need to create past, present and future class's
 // currentTimeBlockHour is text area used to compare to now and set true false
 // compare now integer and currentTimeBlockHour integer to get boolean 
 
 // use boolean to set present and past in if statement with else to set background
-  // if now === currentTimeBlockHour set background-color: #ff6961 maybe a way to do this with arrow function (present)
-  // if now != currentTimeBlockHour set background-color: #d3d3d3 (past)
-  // else set background-color: #77dd77 (future)
+for (timeBlockHour) {
+  if (now === currentTimeBlockHour){
+    document.getElementById().style.backgroundColor = "#ff6961"
+  } else if (now != currentTimeBlockHour) {
+    document.getElementById().style.backgroundColor = "#d3d3d3"
+  } else{
+    document.getElementById().style.backgroundColor = "#77dd77"
+  };
+}  
   
+    
+  
+
+// let past = document.getElementById("").style.backgroundColor=#d3d3d3;
+// let present = document.getElementById("").style.backgroundColor=#ff6961
+// let future = document.getElementById().style.backgroundColor = #77dd77
 
 
  

15c9eb5b1b4c28bf623967e86d0eb5f04d397e00
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-05 15:11:06 -0600

    functionality done need to update readme

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var today = dayjs();
 $('#currentDay').text(dayjs().format('MMM D, YYYY h:mm A'));
-//  now gets current time parse int turns dayjs sting to integer .format gets time in 24hr time
+
 var now = parseInt(dayjs().format("H"));
   console.log(now);    
-//  querySelectorAll calls all id's with time-block and assigns them to  var colorElements
+
 var colorElements = $(".time-block");
 console.log(colorElements);  
-// split method removed first part of id and only returns number as an array of string's, parseInt takes array of string's and makes them an array of integers in a new variable. Once currentTimeBlockHour was console.log found two digit integers logging last digit 10=0 11=1 etc while now is console logging 1=13 2=14 etc. When anything other than position 1 is passed into parseInt returns NaN in console, if 00 is added to id in html logged integer does not change.
+
 colorElements.each(function(i){
   var timeBlockHour = $(this).attr("id")
   timeBlockHour = timeBlockHour.split("-")[1]
@@ -24,25 +24,24 @@ colorElements.each(function(i){
   }; 
 })
 
+loadInfo();
+
 $(".saveBtn").on("click", function (){
   var textInput = $(this).siblings("textarea").val();
   var timePlanner = $(this).parent().attr("id");
   localStorage.setItem(timePlanner, textInput);
-})
-// display saved input to text area from locale storage
+});
 
-console.log("timePlanner");
-console.log("textInput");
-
-$("#hour-9").children("textarea").val(localStorage.getItem("#hour-9"));
-$("#hour-10").children("textarea").val(localStorage.getItem("#hour-10"));
-$("#hour-11").children("textarea").val(localStorage.getItem("#hour-11"));
-$("#hour-12").children("textarea").val(localStorage.getItem("#hour-12"));
-$("#hour-1").children("textarea").val(localStorage.getItem("#hour-1"));
-$("#hour-2").children("textarea").val(localStorage.getItem("#hour-2"));
-$("#hour-3").children("textarea").val(localStorage.getItem("#hour-3"));
-$("#hour-4").children("textarea").val(localStorage.getItem("#hour-4"));
-$("#hour-5").children("textarea").val(localStorage.getItem("#hour-5"));
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+function loadInfo() {
+  
+  console.log(localStorage.getItem("hour-11"))
+  $('#hour-09 .description').val(localStorage.getItem('hour-09'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+}
